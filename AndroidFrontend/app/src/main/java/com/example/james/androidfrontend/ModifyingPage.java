4c75f102ebfbfcc1c3121fc8f319c459f90a4e10
jameszdl <44753880+jameszdl@users.noreply.github.com>
2018-11-06 18:15:40 -0500

    android first commit

---
+++ AndroidFrontend/app/src/main/java/com/example/james/androidfrontend/ModifyingPage.java
@@ -0,0 +1,13 @@
+package com.example.james.androidfrontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifying_page);
+    }
+}
