2beb1cc77e86cbc59dd49e6bdd815865f0fd6943
ryanxie0 <chickenfeet300@gmail.com>
2018-11-18 13:43:35 -0500

    JUnit Test Implementation
    Set up JUnit testing for one of the AWS Lambda functions.

--- aws-service/StockerCloud/src/main/java/com/stockercloud/aws/item/BaseHandler.java
+++ aws-service/StockerCloud/src/main/java/com/stockercloud/aws/item/BaseHandler.java
@@ -1,23 +1,15 @@
 package com.stockercloud.aws.item;
 
-//import com.amazonaws.regions.Region;
-//import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.stockercloud.aws.api.DynamoDBHelper;
 
 public class BaseHandler {
 	
-	protected DynamoDB dynamoDb;
-    protected static final String DYNAMODB_TABLE_NAME = "inventory_items";
-//    private static final Regions REGION = Regions.US_EAST_2;
+	protected DynamoDBHelper helper = new DynamoDBHelper();
+    public static final String DYNAMODB_TABLE_NAME = "inventory_items";
 
 	protected Table getItemTable()
 	{
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-//      client.setRegion(Region.getRegion(REGION));
-        dynamoDb = new DynamoDB(client);
-		return dynamoDb.getTable(DYNAMODB_TABLE_NAME);
+        return helper.getTable(DYNAMODB_TABLE_NAME);
 	}
 }
